40330d75d4fa91bcde149bafd4c55c3f711c6ad2
Abhishek Choudhary <abhishekchoudharysc@gmail.com>
2025-06-02 22:13:29 +0530

    19- Done

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -5,8 +5,6 @@ const Demo = () => {
   const [text, setText] = useState(0);
   const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  // console.log("Rendering");
-
   const prime = useMemo(() => findPrime(text), [text]);
   return (
     <div
